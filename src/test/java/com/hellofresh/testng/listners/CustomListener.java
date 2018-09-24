cdfbc3a2ff25bdf33d23867f59c734b1f2b19bb8
garima <garima.120216@gmail.com>
2018-09-24 17:04:06 +0200

    adding changes

--- src/test/java/com/hellofresh/testng/listners/CustomListener.java
+++ src/test/java/com/hellofresh/testng/listners/CustomListener.java
@@ -11,13 +11,13 @@ public class CustomListener extends BaseSeleniumSetup implements ITestListener{
 
 	@Override
 	public void onTestStart(ITestResult result) {
-		log.info("Test Start------"+result.getMethod().getMethodName());
+		//log.info("Test Start------"+result.getMethod().getMethodName());
 		
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		log.info("Test Passed-----"+result.getMethod().getMethodName());
+		//log.info("Test Passed-----"+result.getMethod().getMethodName());
 		
 	}
 
@@ -39,13 +39,13 @@ public class CustomListener extends BaseSeleniumSetup implements ITestListener{
 
 	@Override
 	public void onStart(ITestContext context) {
-        log.info("Start Of Execution(TEST)->"+ context.getName());
+        //log.info("Start Of Execution(TEST)->"+ context.getName());
 		
 	}
 
 	@Override
 	public void onFinish(ITestContext context) {
-        log.info("End Of Execution(TEST)->"+ context.getName());
+        //log.info("End Of Execution(TEST)->"+ context.getName());
 		
 	}
 
